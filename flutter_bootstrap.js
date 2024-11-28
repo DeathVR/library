83839fd1a395e1658f0ccf685e9f471c1393be6e
DeathVR <64973519+DeathVR@users.noreply.github.com>
2024-11-28 10:31:24 +0500

    update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4240650304"
+    serviceWorkerVersion: "1771750962"
   }
 });
